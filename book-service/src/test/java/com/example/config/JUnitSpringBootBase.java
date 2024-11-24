9ec8b40cccc0b9ff969c664b6f1edaae59a2db91
kruto <krutoi.chuvak1918@gmail.com>
2024-11-24 16:40:15 +0300

    SpringJUnit_HW

--- book-service/src/test/java/com/example/config/JUnitSpringBootBase.java
+++ book-service/src/test/java/com/example/config/JUnitSpringBootBase.java
@@ -2,10 +2,24 @@ package com.example.config;
 
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class JUnitSpringBootBase {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(d -> d.disable())
+                .authorizeHttpRequests(s -> s
+                        .anyRequest()
+                        .permitAll())
+                .build();
+    }
+
 }
